23bd09c98714bae1cc7314e50c8153015135ec0d
Maxim Bernatsky <maxsateon@yandex.ru>
2023-03-07 13:57:51 +0300

    add packages and start/dev scripts

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const { PORT = 3000 } = process.env;
 
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('hello');
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
