53637c8c65f4968d3ba508fd62b36134f8b56cee
Maxim Bernatsky <maxsateon@yandex.ru>
2023-03-09 17:50:33 +0300

    rework cards controllers

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,6 +2,7 @@ const Card = require('../models/card');
 
 const getCards = (req, res) => {
   Card.find({})
+    .populate(['owner', 'likes'])
     .then((cards) => {
       res.status(200).send(cards);
     })
@@ -10,17 +11,17 @@ const getCards = (req, res) => {
         res.status(400).send({
           message: 'Переданы некорректные данные при поиске карточек',
         });
-      } else {
-        res.status(500).send({
-          message: 'Ошибка по умолчанию',
-        });
+        return;
       }
+      res.status(500).send({
+        message: 'Ошибка по умолчанию',
+      });
     });
 };
 
 const createCard = (req, res) => {
   const { name, link } = req.body;
-  Card.create({ name, link })
+  Card.create({ name, link, owner: req.user._id })
     .then((card) => {
       res.status(200).send(card);
     })
@@ -29,20 +30,30 @@ const createCard = (req, res) => {
         res.status(400).send({
           message: 'Переданы некорректные данные при создании карточки',
         });
-      } else {
-        res.status(500).send({
-          message: 'Ошибка по умолчанию',
-        });
+        return;
       }
+      res.status(500).send({
+        message: 'Ошибка по умолчанию',
+      });
     });
 };
 
 const deleteCard = (req, res) => {
   Card.findByIdAndDelete(req.params.cardId)
-    .then(() => {
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Карточка не найдена' });
+        return;
+      }
       res.status(200).send({ message: 'Карточка успешно удалена' });
     })
     .catch((error) => {
+      if (error.name === 'CastError') {
+        res
+          .status(400)
+          .send({ message: 'Ошибка при передачи данных о карточке' });
+        return;
+      }
       res.status(500).send(`Произошла ошибка: ${error}`);
     });
 };
@@ -53,10 +64,20 @@ const likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
-    .then(() => {
-      res.status(200).send({ message: 'Лайк успешно добавлен' });
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Карточка не найдена' });
+        return;
+      }
+      res.status(200).send({ message: 'Карточка успешно удалена' });
     })
     .catch((error) => {
+      if (error.name === 'CastError') {
+        res
+          .status(400)
+          .send({ message: 'Ошибка при передачи данных о карточке' });
+        return;
+      }
       res.status(500).send(`Произошла ошибка: ${error}`);
     });
 };
@@ -67,10 +88,20 @@ const dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } },
     { new: true },
   )
-    .then(() => {
-      res.status(200).send({ message: 'Лайк успешно удален' });
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Карточка не найдена' });
+        return;
+      }
+      res.status(200).send({ message: 'Карточка успешно удалена' });
     })
     .catch((error) => {
+      if (error.name === 'CastError') {
+        res
+          .status(400)
+          .send({ message: 'Ошибка при передачи данных о карточке' });
+        return;
+      }
       res.status(500).send(`Произошла ошибка: ${error}`);
     });
 };
